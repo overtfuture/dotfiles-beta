fe34e8ee7cc3ea0d2680a0460febfe026c1b0920
Nathan Grey <nathan.grey@megaport.com>
2022-04-25 11:45:03 -0400

    Updates neovim config

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -41,6 +41,7 @@ return require('packer').startup(function(use)
     -- Autocomplete
     use {
         "hrsh7th/nvim-cmp",
+        config = lua_path "cmp",
         requires = {{'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-vsnip'}, {'hrsh7th/vim-vsnip'},
                     {'hrsh7th/cmp-path'}, {'hrsh7th/cmp-calc'}, {'hrsh7th/cmp-cmdline'}}
     }
