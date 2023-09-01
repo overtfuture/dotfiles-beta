ed70c469f5971a8dcbac581fcbe93f7f21b80af5
Nathan Grey <nate@grey.bz>
2023-09-01 10:30:15 -0400

    Golang updates

--- private_dot_config/nvim/lua/plugins/language-go.lua
+++ private_dot_config/nvim/lua/plugins/language-go.lua
@@ -9,6 +9,15 @@ return {
         require("go").setup({})
         require("go.format").goimport()  -- goimport + gofmt
         require "configs.lsp-go"
+
+        local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+                require('go.format').goimport()
+            end,
+            group = format_sync_grp,
+        })
     end,
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
